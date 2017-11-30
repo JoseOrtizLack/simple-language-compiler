c51ce6c2942861d7a25a4868faba724701da5a8a
Jose Ortiz <joseortizlack@outlook.com>
2017-11-30 13:59:59 -0600

    updated headRef parameter name to head for clarity and simplicity, removed the type parameter because of redundancy and added the insertFloatSymbol and insertIntegerSymbol method definitions

--- symbolTable.h
+++ symbolTable.h
@@ -6,6 +6,7 @@
 
 #ifndef __SYMBOL_TABLE_H__
 #define __SYMBOL_TABLE_H__
+
 /**
  * @brief The symbol type
  */
@@ -36,37 +37,56 @@ typedef struct tagSymbol {
 
 /**
  * @brief inserts a new integer symbol at the end of the list
- * @param headRef reference to the head of the table
+ * @param head reference to the head of the table
  * @param type type of symbol to be added
  * @param identifier identifier of the symbol to be added
-  * @return  0 if there was an error adding the symbol, 1 if the symbol was added successfully, or 2 if the symbool already exists
+  * @return  0 if there was an error adding the symbol, 1 if the symbol was added successfully, or 2 if the symbol already exists
  */
-int insertIntegerSymbol(Symbol **headRef, SymbolType type, char *identifier, int value);
+int insertIntegerSymbol( Symbol **head, char *identifier, int value );
 
 /**
  * @brief inserts a new float symbol at the end of the list
- * @param headRef reference to the head of the table
+ * @param head reference to the head of the table
  * @param type type of symbol to be added
  * @param identifier identifier of the symbol to be added
  * @param value float value of the symbol to be added
- * @return  0 if there was an error adding the symbol, 1 if the symbol was added successfully, or 2 if the symbool already exists
+ * @return  0 if there was an error adding the symbol, 1 if the symbol was added successfully, or 2 if the symbol already exists
  */
-int insertFloatSymbol(Symbol **headRef, SymbolType type, char *identifier, int value);
+int insertFloatSymbol( Symbol **head, char *identifier, float value );
 
 /**
  * @brief finds the last symbol in the list
- * @param headRef reference to the head of the table
+ * @param head reference to the head of the table
  * @return the last symbol or NULL if there is no symbols in the table
  */
-Symbol *getLastSymbol(Symbol **headRef);
+Symbol *getLastSymbol( Symbol **head );
 
 /**
  * @brief searches a symbol by identifier
- * @param headRef reference to the head of the table
+ * @param head reference to the head of the table
  * @param identifier identifier of the symbol
  * @return the searched symbol or NULL if there is no symbol with this identifier
  */
-Symbol *findSymbol(Symbol **headRef, char *identifier);
+Symbol *findSymbol( Symbol **head, char *identifier );
+
+
+/**
+ * @brief updates the value of an integer symbol
+ * @param head reference to the head of the table
+ * @param identifier identifier of the symbol
+ * @param newValue updated value of the symbol
+ * @return 0 if the symbol was not found, 1 if the update was sucessful
+ */
+int updateIntegerSymbol( Symbol **head, char *identifier, int newValue );
+
+/**
+ * @brief updates the value of an integer symbol
+ * @param head reference to the head of the table
+ * @param identifier identifier of the symbol
+ * @param newValue updated value of the symbol
+ * @return 0 if the symbol was not found, 1 if the update was sucessful
+ */
+int updateFloatSymbol( Symbol **head, char *identifier, float newValue );
 
 #endif //__SYMBOL_TABLE_H__
 
